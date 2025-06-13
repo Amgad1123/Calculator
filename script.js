ca2a858a97bc15630e6f87d16b263828140aacd7
Amgad1123 <Amgadahmed2303@gmail.com>
2025-06-13 09:52:28 -0500

    Added rounding to 3 decimal places.

--- script.js
+++ script.js
@@ -1,22 +1,89 @@
 const container = document.querySelector('container');
-let number;
-let operator;
-let secondNumber;
+let number = null;
+let operator = 0;
+let secondNumber = null;
+
+const display = document.querySelector('placeholder')
+const buttons = document.querySelectorAll('button');
+const clear = document.querySelector('.clear')
+
+buttons.forEach((button => {
+    button.addEventListener('click', () => {
+        let val = button.textContent;
+        
+        //functionality for the clear button
+        clear.addEventListener("click", () => {
+        number = null;
+        operator = 0;
+        secondNumber = null;
+        document.getElementById('searchBar').value = " ";
+        });
+
+        //gets the operator from the calculator button selection
+        if (number != null && val == "/" ||
+            val == "-" ||
+            val == "+" ||
+            val == "x"
+        ) {
+            operator = val;
+            document.getElementById('searchBar').value += " " + val + " ";
+        }
+        //gets the first number from the user 
+        else if(operator == 0 && val != "/" &&
+            val != "+" && val != "-" && val != "x"
+        ) {
+            if( number == null) {
+                number = val;
+                document.getElementById('searchBar').value = val;
+            }
+            else {
+                number += val;
+                document.getElementById('searchBar').value += val;
+            }
+        }
+        else if(number != undefined && operator != 0 && val != '='
+        ) {
+            if (secondNumber == null) {
+                secondNumber = val
+                document.getElementById('searchBar').value += val;
+            } 
+            else {
+            secondNumber += val;
+            document.getElementById('searchBar').value += val;
+        }
+    }
+        /* equates the problem if the equal sign is 
+        selected and other conditons are met*/
+
+        else if(val == '=' && number != undefined &&
+            operator != 0 && secondNumber != undefined
+        ){
+            operate(operator, parseInt(number), parseInt(secondNumber));
+        }
+
+    });
+})) ;
 
 function add(firstNum, secNum) {
-    return firstNum + secNum;
+    document.getElementById('searchBar').value = Math.round((firstNum + secNum)*1000)/1000;
 }
 
 function subtract(firstNum, secNum) {
-    return firstNum - secNum;
+    document.getElementById('searchBar').value = Math.round((firstNum - secNum) * 1000) /1000;
 }
 
 function multiply(firstNum, secNum) {
-    return firstNum * secNum;
+    document.getElementById('searchBar').value = Math.round((firstNum * secNum) * 1000)/1000;
 }
 
 function divide(firstNum, secNum) {
-    return firstNum / secNum;
+    console.log(secNum)
+    if (secNum === 0) {
+        document.getElementById('searchBar').value = 'No Bueno';
+    }
+    else {
+        document.getElementById('searchBar').value = Math.round((firstNum / secNum) * 1000) / 1000;
+}
 }
 
 function operate(operator, num, secNum) {
@@ -26,7 +93,7 @@ function operate(operator, num, secNum) {
     else if (operator === '-') {
         subtract(num, secNum);
     }
-    else if (operator === '*') {
+    else if (operator === 'x') {
         multiply(num, secNum);
     }
     else if (operator === '/') {
